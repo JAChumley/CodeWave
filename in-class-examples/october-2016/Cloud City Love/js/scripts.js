0b6605682df3f786b0f58d8f0d760e09cded6b53
JAChumley <jestaar@gmail.com>
2016-10-27 09:53:12 -0700

    added anotations

--- in-class-examples/october-2016/Cloud City Love/js/scripts.js
+++ in-class-examples/october-2016/Cloud City Love/js/scripts.js
@@ -21,6 +21,7 @@
 
 Make number min to 0
 */
+// Variables are pointers to their values and a means to store data
 let $fromHeight = $('#from-height');
 let $toHeight = $('#to-height');
 let $fromWeight = $('#from-weight');
@@ -32,15 +33,15 @@ let $hairColor = $('#hair-color');
 let $skinColor = $('#skin-color');
 
 let $matchesContainer = $('#matches-container');
-
+  // click() method triggers the click event for find-love-button
 $('#find-love-btn').click(() => {
   
   // load the .gif image when the user clicks on button and append it to the #waiting div
   let imgWait =`<img src="assets/img/bb8.gif" class="wait"><h5 class=text-center>Finding your love!</h5>`;
   $('#waiting').append(imgWait);
-
+  // clears matches container with every new search
   $matchesContainer.empty();
-  
+  // ternary operator used to compare values of inputs and uses variances to identify matches  
   let fromHeight = $fromHeight.val() ? Number($fromHeight.val()) : 0;
   let toHeight = $toHeight.val() ?  Number($toHeight.val()) : Number.POSITIVE_INFINITY;
   let fromWeight =  Number($fromWeight.val()) ? $fromWeight.val() : 0;
@@ -50,18 +51,18 @@ $('#find-love-btn').click(() => {
   let gender = $gender.val();
   let hairColor = $hairColor.val() ? $hairColor.val() : 'any';
   let skinColor = $skinColor.val() ? $skinColor.val() : 'any';
-  
+  // sets variable for storing an array of matches
   let matches = [];
-  
+  // the 'for' loop is used making the function call and sorting through the array
   for (let i = 1; i <= 9; i++) {
-    
+  // getJSON method is used to get JSON data using an AJAX HTTP GET request
     $.getJSON('http://swapi.co/api/people/?format=json&page=' + i, data => {
-      
+  // variable set for storing results once sorted   
       let people = data.results;
       
       // console.log(people);
       // check for matching inputs
-      
+  // function used to sort JSON array according to parameters set by user input    
       people.forEach(person => {
         if (person.height >= fromHeight && person.height <= toHeight
           && person.mass >= fromWeight && person.mass <= toWeight) {
@@ -77,7 +78,7 @@ $('#find-love-btn').click(() => {
                 // check skin color
                 if (skinColor === 'any'
                 || person.skin_color.includes(skinColor)) {
-                  
+         // results or matches are "push"ed into an array of matches         
                   matches.push(person);
                   
                 } // / skin color
@@ -102,7 +103,7 @@ $('#find-love-btn').click(() => {
       console.log(matches);
       
       // need to check if matches is empty to display something like "No match was found"
-      
+      // if statement used to post alternative text if no matches are found
       if(matches.length === 0){
         $matchesContainer.append(`
           <h3 class="no-match-title">Not even in a galaxy far far away there's a match for you!</h3>
@@ -112,7 +113,7 @@ $('#find-love-btn').click(() => {
           
           // put matches on screen
           matches.forEach(person => {
-            
+         // this function formats the resulting statistics of match   
             let name = person.name;
             // TODO: get image url
             // let imgURL =
@@ -121,10 +122,10 @@ $('#find-love-btn').click(() => {
             let weight = person.mass;
             let hairColor = person.hair_color;
             let skinColor = person.skin_color;
-            
+           // matches weight and height are converted from metric to standard for a more enjoyable experience 
             let weightLb = kilogramsToPounds(weight);
             let heightFeet = cmToFeet(height);
-            
+          // manipulating the DOM injecting javascript into html linked to div with id='matches-container'  
             $matchesContainer.append(`
               <h2>${name}</h2>
               <p><small>${gender}</small></p>
@@ -159,7 +160,7 @@ $('#find-love-btn').click(() => {
       $('#search-by-name-btn').on('click', () => {
         displayResult();
       });
-      
+      // 'Enter' key binding enabled for search function
       $('#query').keypress(e => {
         if (e.key === 'Enter') displayResult();
       });
